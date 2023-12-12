aa3f32942ee4f0778683af78382ad660fbbd203a
fakhri <fakhri.fironus@tmrnd.com.my>
2023-12-12 12:22:36 +0800

    🔥 remove unused exception class

--- lib/keycloak_wrapper.dart
+++ lib/keycloak_wrapper.dart
@@ -11,6 +11,5 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 part 'src/config.dart';
 part 'src/constants.dart';
-part 'src/exceptions.dart';
 part 'src/helpers.dart';
 part 'src/wrapper.dart';
